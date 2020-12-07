4b42a2539087713ec83cab3d5a93b8033be7fb08
tianci_aiera <tianci_aiera@xiewenfeideMacBook-Air.local>
2020-12-07 15:46:02 +0800

    committed first script main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+'''breaking is an tech news scrawler that automatically collecting today's tech news from given sites. Then group and ranking news pages according to key words. Aims to reduce the repetitive labour for AI era's editors'''
+
+def main():
+    pass
